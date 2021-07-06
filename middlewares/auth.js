c610c17c593dbd8d7078d0706e2520324337e69b
Roman SP <prs@m-cti.ru>
2021-07-06 14:20:09 +0300

    initial+clone mesto

---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/unauthorized');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization && !authorization.startsWith('Bearer')) {
+    throw new UnauthorizedError('Войдите на сайт');
+  }
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
+  } catch (err) {
+    throw new UnauthorizedError('Войдите на сайт');
+  }
+  req.user = payload;
+  next();
+};
---
+++ routes/cards.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const isUrl = require('validator/lib/isURL');
+
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
+
+router.get('/cards', getCards);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().custom((value, helpers) => {
+      if (isUrl(value, { require_protocol: true })) {
+        return value;
+      }
+      return helpers.message('Неверная ссылка');
+    }),
+  }),
+}), createCard);
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), likeCard);
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), dislikeCard);
+
+module.exports = router;
